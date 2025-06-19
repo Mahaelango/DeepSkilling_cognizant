b8f5a79b10d21c5d2263855008e4c3b81017e160
Mahalakshmi E <118118180+Mahaelango@users.noreply.github.com>
2025-06-19 21:26:00 +0530

    week 1 - solutions for practice problems

---
+++ week1/Design_Pattern_Principles/Ex_2_FactoryMethodPatternExample/src/ExcelFactory.java
@@ -0,0 +1,7 @@
+package FactoryMethodPattern;
+
+public class ExcelFactory extends DocumentFactory {
+    public Document createDocument() {
+        return new ExcelDocument();
+    }
+}
---
+++ week1/Design_Pattern_Principles/Ex_2_FactoryMethodPatternExample/src/PdfDocument.java
@@ -0,0 +1,7 @@
+package FactoryMethodPattern;
+
+public class PdfDocument implements Document {
+    public void open() {
+        System.out.println("Opening a PDF document.");
+    }
+}
